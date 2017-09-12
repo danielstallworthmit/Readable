6983f4d2a639928790fb3410925bc7c579fe5c8b
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-11 20:12:37 -0700

    Set up index.js using react-router-dom to appropriate components to render. Can render two components for post and list of posts and filter for category within the component

--- readable/src/index.js
+++ readable/src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import promise from 'redux-promise';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Post from './components/post'
+import MainIndex from './components/main_index'
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <Switch>
+                <Route path='/:category/:post_id' component={Post} />
+                <Route path='/:category' component={MainIndex} />
+                <Route path='/' component={MainIndex} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
