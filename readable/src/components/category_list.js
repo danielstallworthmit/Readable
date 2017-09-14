b8a84ec03780662e8c5b78a9f28a3254173eb007
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-14 16:36:23 -0700

    Update state when category route changes to reflect route

--- readable/src/components/category_list.js
+++ readable/src/components/category_list.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { fetchCategoryPosts } from '../actions';
+import { connect } from 'react-redux';
 
-export default (props) => {
-    return (
-        <ul className="categoryList">
-               { props.cats.map(cat => (
-                   <Link to={`/${cat}`}>
-                        <li key={cat}>
-                            {cat}
-                        </li>
-                    </Link>
-                ))
-               }
-        </ul>
-    )
+class CategoryList extends React.Component {
+    categoryRender() {
+        this.props.fetchCategoryPosts(this.props.match.params.category)
+    }
+
+    render() {
+        return (
+            <ul className="categoryList">
+                   { this.props.cats.map(cat => (
+                       <Link to={`/${cat}`} onClick={this.categoryRender}>
+                            <li key={cat}>
+                                {cat}
+                            </li>
+                        </Link>
+                    ))
+                   }
+            </ul>
+        )
+    }
 }
+
+export default withRouter(connect(null, { fetchCategoryPosts })(CategoryList));
