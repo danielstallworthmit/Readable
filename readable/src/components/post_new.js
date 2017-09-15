60e15ca598bc86e5500d029b84cd65a707bdef87
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-14 19:12:49 -0700

    Add a post up and running smoothly thanks to redux form

--- readable/src/components/post_new.js
+++ readable/src/components/post_new.js
@@ -5,11 +5,84 @@ import { connect } from 'react-redux';
 import { createPost } from '../actions'; 
 
 class PostNew extends React.Component {
-    render() {
+    renderField(field) {
+        const { meta: { touched, error } } = field;
         return (
-            <h1>Hi</h1>
+            <div>
+                <label>{field.label}</label>
+                {field.type === 'text' ?
+                    <input type='text' {...field.input} />
+                    :
+                    <textarea {...field.input} />
+                }
+                <div className="errorMessage">
+                    {touched ? error : ''}    
+                </div>
+            </div>
+        )
+    }
+
+    onSubmit(entity) {
+        console.log(entity);
+        this.props.createPost(entity, () => {
+            this.props.history.push(`/${entity.category}/${entity.id}`);
+        })
+    } 
+    render() {
+        const { handleSubmit } = this.props;
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field 
+                    label="Title"
+                    type="text"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Author"
+                    type="text"
+                    name="author"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Category"
+                    type="text"
+                    name="category"
+                    component={this.renderField}
+                />
+                <Field 
+                    label="Body"
+                    type="textarea"
+                    name="body"
+                    component={this.renderField}
+                />
+                <button className="submitButton" type="submit">Submit</button>
+                <Link to='/' className="cancelButton">Cancel</Link>
+            </form>
         )
     }
 }
 
-export default withRouter(connect(null, { createPost })(PostNew));
+const validate = (vals) => {
+    const errors = {};
+    if (!vals.title) {
+        errors.title = "Please enter a title."
+    }
+    if (!vals.category) {
+        errors.category = "Please enter a category."
+    }
+    if (!vals.author) {
+        errors.author = "Please enter your name."
+    }
+    if (!vals.body) {
+        errors.body = "Please enter some text."
+    }
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostNewForm'
+})(
+    withRouter(connect(null, { createPost })(PostNew))
+)
